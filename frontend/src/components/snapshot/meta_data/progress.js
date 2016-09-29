2c94254dbcd21270cb8e77b9e715e55333d3e569
server-monitor <server.monitor.lizard@gmail.com>
2016-09-29 04:42:02 -0700

    Lint src/components/snapshot/meta_data/progress.js

--- frontend/src/components/snapshot/meta_data/progress.js
+++ frontend/src/components/snapshot/meta_data/progress.js
@@ -5,15 +5,13 @@ import 'rc-slider/assets/index.css';
 import RCSlider from 'rc-slider';
 import './progress.less';
 
-export default class Progress extends React.Component {
-  render() {
-    return (
-      <div className='snapshot_meta_data_progress'>
-        <div className='slider_container'>
-          <div>Track your progress</div>
-          <RCSlider />
-        </div>
-      </div>
-    );
-  }
-}
+const Progress = () => (
+  <div className="snapshot_meta_data_progress">
+    <div className="slider_container">
+      <div>Track your progress</div>
+      <RCSlider />
+    </div>
+  </div>
+);
+
+export default Progress;
