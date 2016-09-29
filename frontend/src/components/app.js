4a13d8b648c5468c3b6db2b76c06ec843de34a87
server-monitor <server.monitor.lizard@gmail.com>
2016-09-29 03:20:23 -0700

    Lint src/components/app.js

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 // Is there a way to not include this for Stardust or is this really necessary?
 import 'semantic-ui-css/semantic.css';
@@ -7,14 +7,12 @@ import Menu from '../components/menu';
 import SnapshotList from '../containers/snapshot/list';
 import Status from '../containers/status';
 
-export default class App extends Component {
-  render() {
-    return (
-      <div>
-        <SnapshotList />
-        <Menu />
-        <Status />
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div>
+    <SnapshotList />
+    <Menu />
+    <Status />
+  </div>
+);
+
+export default App;
