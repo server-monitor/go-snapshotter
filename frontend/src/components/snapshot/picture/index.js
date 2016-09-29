765f1d99cd7390461e7c29373434c00bb25e1b05
server-monitor <server.monitor.lizard@gmail.com>
2016-09-29 04:48:32 -0700

    Lint src/components/snapshot/picture/index.js

--- frontend/src/components/snapshot/picture/index.js
+++ frontend/src/components/snapshot/picture/index.js
@@ -1,24 +1,22 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 import { Image } from 'stardust';
 
 import style from './index.less';
 
-export default class Index extends React.Component {
-  render() {
-    return (
-      <div className={ style.picture_container }>
-        <Image
-          src={ this.context.picture.path }
-          href={ this.context.url }
-          target='_blank'
-          fluid
-        />
-      </div>
-    );
-  }
-}
+const Index = (_, context) => (
+  <div className={style.picture_container}>
+    <Image
+      src={context.picture.path}
+      href={context.url}
+      target="_blank"
+      fluid
+    />
+  </div>
+);
 
 Index.contextTypes = {
-  picture: React.PropTypes.object.isRequired,
-  url: React.PropTypes.string.isRequired,
+  picture: PropTypes.object.isRequired,
+  url: PropTypes.string.isRequired,
 };
+
+export default Index;
